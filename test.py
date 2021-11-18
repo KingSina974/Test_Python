4158fa16cd0823688c833ae92f064f642f5ebb40
kovalen <kovalen-dimitri.ramaretty@epitech.eu>
2021-11-18 15:44:21 +0400

    first push

---
+++ test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+#Part 0
+print("Success\n")
+
+#Part 1 Step 1.1 -Variables
+fruit = "banana"
+quantity = 3
+pie_crust = "empty"
+isOvenOn = False
+
+print("You have", quantity, fruit,"and the pie crust is", pie_crust,"is the oven on ?", isOvenOn,'\n')
+
+#Exercise 1.1.1
+fruit = "apples"
+quantity = 5
+pie_crust = "empty"
+isOvenOn = True
+
+print("You have", quantity, fruit,"and the pie crust is", pie_crust,"is the oven on ?", isOvenOn,'\n')
+
+#Step 1.2 -Functions
+
+
